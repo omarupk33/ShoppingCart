bc8352eb6e52d4b683012e58e78487cb8eecea49
omarupk33 <kkomar12334@gmail.com>
2026-09-02 16:30:59 +0300

    Set up and layout  a simple shop

---
+++ my-react-app/src/Shop/Cards.jsx
@@ -0,0 +1,29 @@
+function Cards({itemList =[]}){
+    return (
+        <div className="cards-container">
+            {itemList.map((item)=>{
+                const img = <h3> {item.name} image</h3>
+                const name = <h3> {item.name}</h3>
+                const info = <h3>{item.info}</h3>
+
+                return (
+                    <div className="card">
+                        {img}
+                        {name}
+                        {info}
+
+                        <button onClick={(e)=>{
+                            e.preventDefault()
+                        }}>
+                            Buy
+                        </button>
+                        
+                    </div>
+                )
+
+            })}
+        </div>
+    )
+}
+
+export default Cards
--- my-react-app/src/Shop/Shop.jsx
+++ my-react-app/src/Shop/Shop.jsx
@@ -1,7 +1,41 @@
+import Cards from './Cards.jsx'
+import './shop.css'
+
+
 const Shop = ()=>{
+
+
+const shoppingList = [
+  {
+    name: "Apple",
+    info: "Fresh red fruit, sold by the pound."
+  },
+  {
+    name: "Milk",
+    info: "Whole cow's milk, 1-gallon carton."
+  },
+  {
+    name: "Bread",
+    info: "Sliced whole wheat bread loaf."
+  },
+  {
+    name: "Eggs",
+    info: "A dozen large grade-A eggs."
+  },
+  {
+    name: "Coffee",
+    info: "Medium roast ground coffee beans."
+  },
+    {
+    name: "Cheese",
+    info: "A french Cheese made of Lamp's milk."
+  }
+];
+
+
     return (
-    <div>
-        Yes, I'm a Shop
+    <div className='shop'>
+       <Cards itemList={shoppingList}></Cards>
     </div>
     )
 } 
